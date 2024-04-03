74b8e3419730b300598d66b4ff70123f1c0c0fd7
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-04-02 21:41:39 -0400

    completed working of turtle to write names of states but still need to work on invalid entries made by user.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import csv
-import math
+import turtle
+
 import pandas
-with (open("weather_data.csv") as data_file):
-    data = pandas.read_csv(data_file)
+from turtle import Turtle,Screen
 
-    monday_temp = data[data.day == "Monday"]
-    print((monday_temp.temp * 9 / 5) + 32)
-    # average = sum(df)/len(df)
-    # print(df.max())
+screen = Screen()
 
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
+
+turtle = Turtle()
+
+data = pandas.read_csv("50_states.csv")
+print(data["state"].item())
+for _ in range(len(data)):
+    user_state_name = screen.textinput("US States", "Enter the name of State:")
+    new_state = data[data.state == user_state_name]
+
+    new_x = new_state.x.item()
+    new_y = new_state.y.item()
+
+
+    turtle.penup()
+    turtle.hideturtle()
+    turtle.goto(x=new_x,y=new_y)
+    turtle.write(user_state_name)
+
+screen.mainloop()
+screen.exitonclick()
