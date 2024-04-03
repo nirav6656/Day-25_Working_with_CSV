d31f51cc08fa18507c24319d378fecb1cc8b02b3
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-04-02 22:38:54 -0400

    completed final project.

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
 turtle = Turtle()
-
+turtle.hideturtle()
 data = pandas.read_csv("50_states.csv")
-print(data["state"].item())
+
 for _ in range(len(data)):
     user_state_name = screen.textinput("US States", "Enter the name of State:")
-    new_state = data[data.state == user_state_name]
+    if user_state_name in data.values:
+        new_state = data[data.state == user_state_name]
 
-    new_x = new_state.x.item()
-    new_y = new_state.y.item()
+        new_x = new_state.x.item()
+        new_y = new_state.y.item()
 
 
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.goto(x=new_x,y=new_y)
-    turtle.write(user_state_name)
+        turtle.penup()
 
+        turtle.goto(x=new_x,y=new_y)
+        turtle.write(user_state_name,font=("Arial", 8, "bold"))
+    else:
+        continue
 screen.mainloop()
 screen.exitonclick()
